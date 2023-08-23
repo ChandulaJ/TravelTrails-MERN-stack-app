987e82fa403e936011ffda9b4a8b05740edb0846
ChandulaJ <114409648+ChandulaJ@users.noreply.github.com>
2023-08-23 14:52:21 +0530

    initial setups done for backend

--- TravelTrails/backend/server.js
+++ TravelTrails/backend/server.js
@@ -1,8 +1,20 @@
+require ('dotenv').config()
 const express = require('express')
 
 const app = express()
 
-app.listen(4000, () => {
-    console.log('listening on port 4000')
+//middleware
+app.use((req,res,next)=>{
+    console.log(req.path,req.method)
+    next()
 })
 
+app.get('/', (req, res) => {
+    res.json({mssg:'Welcome to the app'})
+})
+app.listen(process.env.PORT, () => {
+    console.log('listening on port ',process.env.PORT)
+})
+
+
+process.env
