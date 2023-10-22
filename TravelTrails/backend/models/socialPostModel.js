a87d45b787579f331d7fa689ad6083893d204112
ChandulaJ <114409648+ChandulaJ@users.noreply.github.com>
2023-10-22 11:48:14 +0530

    socialposts nota appearing

--- TravelTrails/backend/models/socialPostModel.js
+++ TravelTrails/backend/models/socialPostModel.js
@@ -1,50 +1,50 @@
-const mongoose = require('mongoose')
-const Schema  = mongoose.Schema
+    const mongoose = require('mongoose')
+    const Schema  = mongoose.Schema
 
-const socialPostSchema = new Schema({
- 
-
-    contentText:{
-        type:String,
-        required:true
-    },
-
-     photo:{
-        type:String,
-        required:false
-     },
-   
-     user_id:{
-            type:String,
-            required:true
-     },
-        username_id:{
-            type:String,
-            required:false
-        },
-        user_address:{
-            type:String,
-            required:false
-        },
-        photoPath:{
-            type:String,
-            required:false
-        },
-        comments: [
-            {
-                comment_text: {
-                    type: String,
-                    required: false
-                },
-                comment_accountId: {
-                    type: String,
-                    required: false
-                }
-            }
-        ]
+    const socialPostSchema = new Schema({
     
 
-},
-{timestamps:true})
+        contentText:{
+            type:String,
+            required:true
+        },
 
-module.exports = mongoose.model('socialPost',socialPostSchema)
+        photo:{
+            type:String,
+            required:false
+        },
+    
+        user_id:{
+                type:String,
+                required:true
+        },
+            username_id:{
+                type:String,
+                required:false
+            },
+            user_address:{
+                type:String,
+                required:false
+            },
+            photoPath:{
+                type:String,
+                required:false
+            },
+            comments: [
+                {
+                    comment_text: {
+                        type: String,
+                        required: false
+                    },
+                    comment_accountId: {
+                        type: String,
+                        required: false
+                    }
+                }
+            ]
+        
+
+    },
+    {timestamps:true})
+
+    module.exports = mongoose.model('socialPost',socialPostSchema)
