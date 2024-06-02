23a9f4f3824236d762353a9aa4f8488b9882d9fe
ChandulaJ <chandugoods3000@gmail.com>
2024-06-02 18:18:41 +0530

    changed localhost to backend

--- TravelTrails/frontend/src/components/SocialPostForm.js
+++ TravelTrails/frontend/src/components/SocialPostForm.js
@@ -1,112 +1,100 @@
-import React,{useState} from 'react'
-import ConvertToBase64 from './convertTo64base'
-import {useSocialPostsContext} from '../hooks/useSocialPostsContext'
-import axios from 'axios'
-import {useAuthContext} from '../hooks/useAuthContext'
-import { set } from 'date-fns'
+import React, { useState } from "react";
+import ConvertToBase64 from "./convertTo64base";
+import { useSocialPostsContext } from "../hooks/useSocialPostsContext";
+import axios from "axios";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { set } from "date-fns";
 
-const SocialPostForm =()=>{
-    
-    const{accounts}=useAuthContext()
-    const[error,setError]=useState(null)
+const SocialPostForm = () => {
+  const { accounts } = useAuthContext();
+  const [error, setError] = useState(null);
 
-     
+  const [socialPost, setSocialPost] = useState({
+    contentText: "",
+    photo: "",
+    username_id: "",
+  });
 
-      const [socialPost, setSocialPost] = useState({
-        contentText: '',
-        photo: '',
-        username_id:''
-      });
-      
-    const handleSubmit = async(e)=>{
-        e.preventDefault()
-        
-        if(!accounts) {
-            setError('You must be logged in')
-            return
-        }
-        if(!socialPost.contentText){
-            setError('Please enter a post')
-            return
-        }
-        if(!socialPost.photo){
-            setError('Please enter a photo')
-            return
-        }
-        const token = accounts.token;
-        const usernameId = accounts.username;
-        const authorization = `Bearer ${token}`;
-        const { contentText,photo } = socialPost;
-        try {
-          const newPost = await axios.post('http://localhost:4000/api/socialPosts', {
-            contentText,
-            photo,
-            usernameId
-          },{
-            headers:{
-              authorization
-            }
-          });
-          
-          setSocialPost({
-            contentText: '',
-            photo: ''
-          });
-          window.location.reload();
-          
-        } catch (e) {
-          console.log(e.response.data.message);
-     
-        }
-      
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!accounts) {
+      setError("You must be logged in");
+      return;
     }
+    if (!socialPost.contentText) {
+      setError("Please enter a post");
+      return;
+    }
+    if (!socialPost.photo) {
+      setError("Please enter a photo");
+      return;
+    }
+    const token = accounts.token;
+    const usernameId = accounts.username;
+    const authorization = `Bearer ${token}`;
+    const { contentText, photo } = socialPost;
+    try {
+      const newPost = await axios.post(
+        "http://backend:4000/api/socialPosts",
+        {
+          contentText,
+          photo,
+          usernameId,
+        },
+        {
+          headers: {
+            authorization,
+          },
+        }
+      );
 
-    const handleFileUpload = async (e) => {
-      const file = e.target.files[0];
-      const base64 = await ConvertToBase64(file);
-      setSocialPost({ ...socialPost, photo: base64 });
-    };
+      setSocialPost({
+        contentText: "",
+        photo: "",
+      });
+      window.location.reload();
+    } catch (e) {
+      console.log(e.response.data.message);
+    }
+  };
 
-    const handleInput = (e) => {
-     setSocialPost({ ...socialPost, [e.target.name]: e.target.value });
-    };
+  const handleFileUpload = async (e) => {
+    const file = e.target.files[0];
+    const base64 = await ConvertToBase64(file);
+    setSocialPost({ ...socialPost, photo: base64 });
+  };
 
-    return(
-        <form className='create-SocialPost' onSubmit = {handleSubmit}>
-          
-          
+  const handleInput = (e) => {
+    setSocialPost({ ...socialPost, [e.target.name]: e.target.value });
+  };
 
+  return (
+    <form className="create-SocialPost" onSubmit={handleSubmit}>
+      <input
+        className="input"
+        type="text"
+        name="contentText"
+        placeholder="Any travel thoughts..."
+        onChange={handleInput}
+      />
 
+      <input
+        type="file"
+        name="photo"
+        accept=".jpg, .jpeg, .png" // Limit accepted file types
+        placeholder="Post photo"
+        onChange={(e) => handleFileUpload(e)}
+      />
+      <img className="post-photo" src={socialPost.photo} />
 
-            <input
-            className='input'
-            type='text'
-            name='contentText'
-            placeholder='Any travel thoughts...'
-            onChange={handleInput}
-            
-           
-            />
+      <button className="btn" onClick={handleSubmit}>
+        Add post
+      </button>
 
-          <input
-            type="file"
-            name='photo'
-            accept=".jpg, .jpeg, .png" // Limit accepted file types
-            placeholder='Post photo'
-            onChange={(e) => handleFileUpload(e)}
-            
-/>
-<img className="post-photo" src={socialPost.photo}  />
+      {error && <div className="error">{error}</div>}
+    </form>
+  );
+};
 
-
-<button className='btn' onClick={handleSubmit}>Add post</button>
-          
-
-{error && <div className="error">{error}</div>}
-            </form>
-
-    )
-
-}
-
-export default SocialPostForm
+export default SocialPostForm;
