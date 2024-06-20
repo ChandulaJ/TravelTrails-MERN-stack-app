d31143ef8aa45e47d7ab60f65341107f8ae90e28
Chandula Jayathilake <114409648+ChandulaJ@users.noreply.github.com>
2024-06-21 00:29:47 +0530

    Create deploy_server.tf

---
+++ deploy_server.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "us-east-2"
+
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-04b70fa74e45c3917"
+    instance_type = "t2.micro"
+    key_name = "deploy-server-key"
+    security_groups = ["MyDeployServer-sg"]
+    tags = {
+        Name = "DeployServer"
+    }
+}
