3c25ea9761bc7ba42aef60d044c860909a26d470
Chandula Jayathilake <114409648+ChandulaJ@users.noreply.github.com>
2024-06-21 16:06:43 +0530

    edit

--- deploy_server.tf
+++ deploy_server.tf
@@ -1,14 +1,18 @@
-provider "aws" {
-    region = "us-east-1"
 
+provider "aws" {
+  region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-    ami = "ami-04b70fa74e45c3917"
-    instance_type = "t2.micro"
-    key_name = "deploy-server-key"
-    security_groups = ["MyDeployServer-sg"]
-    tags = {
-        Name = "DeployServer"
-    }
+resource "aws_instance" "my-ec2" {
+  ami           = "ami-04b70fa74e45c3917"
+  instance_type = "t2.micro"
+  key_name      = "deploy-server-key"
+
+  tags = {
+    Name = "DeployServer"
+  }
+}
+
+output "instance_ip" {
+  value = aws_instance.my-ec2.public_ip
 }
