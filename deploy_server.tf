4a9417fef698ace14c555809cf6123fb553db51a
Chandula Jayathilake <114409648+ChandulaJ@users.noreply.github.com>
2024-06-21 16:29:48 +0530

    Update deploy_server.tf

--- deploy_server.tf
+++ deploy_server.tf
@@ -7,6 +7,7 @@ resource "aws_instance" "my-ec2" {
   ami           = "ami-04b70fa74e45c3917"
   instance_type = "t2.micro"
   key_name      = "deploy-server-key"
+  vpc_security_group_ids = ["sg-08d9dd0b50b76f3a0"]
 
   tags = {
     Name = "DeployServer"
