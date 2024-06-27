b834b6d083cfa760aebe7726abb27653cc016531
Chandula Jayathilake <114409648+ChandulaJ@users.noreply.github.com>
2024-06-27 11:44:29 +0530

    Update deploy_server.tf

--- deploy_server.tf
+++ deploy_server.tf
@@ -3,17 +3,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "my-ec2" {
+resource "aws_instance" "TravelTrails-ec2" {
   ami           = "ami-04b70fa74e45c3917"
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
   key_name      = "deploy-server-key"
   vpc_security_group_ids = ["sg-08d9dd0b50b76f3a0"]
 
   tags = {
-    Name = "DeployServer"
+    Name = "TravelTrails-DeployServer"
   }
 }
 
-output "instance_ip" {
-  value = aws_instance.my-ec2.public_ip
+resource "aws_eip_association" "TravelTrails-eip_assoc" {
+  instance_id   = aws_instance.TravelTrails-ec2.id
+  allocation_id = "eipalloc-0249d6092bf4d4079"
 }
+
