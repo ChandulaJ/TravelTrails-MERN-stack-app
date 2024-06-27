1d4d46d7231e876baf5bd31bf2587cf140bd6bee
Chandula Jayathilake <114409648+ChandulaJ@users.noreply.github.com>
2024-06-27 14:56:31 +0530

    removed elastic ip

--- deploy_server.tf
+++ deploy_server.tf
@@ -3,23 +3,17 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "TravelTrails-ec2" {
+resource "aws_instance" "my-ec2" {
   ami           = "ami-04b70fa74e45c3917"
-  instance_type = "t2.small"
+  instance_type = "t2.micro"
   key_name      = "deploy-server-key"
   vpc_security_group_ids = ["sg-08d9dd0b50b76f3a0"]
 
   tags = {
-    Name = "TravelTrails-DeployServer"
+    Name = "DeployServer"
   }
 }
 
-resource "aws_eip_association" "TravelTrails-eip_assoc" {
-  instance_id   = aws_instance.TravelTrails-ec2.id
-  allocation_id = "eipalloc-0249d6092bf4d4079"
-}
-
-
 output "instance_ip" {
-  value = aws_instance.TravelTrails-ec2.public_ip
+  value = aws_instance.my-ec2.public_ip
 }
