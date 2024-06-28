d98f99324fe0cf97601d7a28fe5451fe2503e4ec
Chandula Jayathilake <114409648+ChandulaJ@users.noreply.github.com>
2024-06-28 21:31:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,17 @@ resource "aws_instance" "my-ec2" {
     Name = "DeployServer"
   }
 }
+
+resource "aws_eip" "my_eip" {
+  # This block is not creating a new EIP, but rather managing an existing EIP
+  depends_on = [aws_instance.my-ec2]
+  instance = aws_instance.my-ec2.id
+  # allocation_id = "eipalloc-03a066f8fdd51aaf6"  # allocation_id should not be set for managing an existing EIP
+}
+
 resource "aws_eip_association" "my_eip_assoc" {
   instance_id   = aws_instance.my-ec2.id
   allocation_id = "eipalloc-03a066f8fdd51aaf6"
-  
 }
 
 output "instance_ip" {
