4138e8e303674aba2c52e615f045be05b60cdf4e
Chandula Jayathilake <114409648+ChandulaJ@users.noreply.github.com>
2024-06-28 21:50:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "my-ec2" {
+resource "aws_instance" "my_ec2" {
   ami           = "ami-0e001c9271cf7f3b9"
   instance_type = "t2.medium"
   key_name      = "traveltrails-deploy-server"
@@ -13,12 +13,15 @@ resource "aws_instance" "my-ec2" {
   }
 }
 
-
-resource "aws_eip_association" "TravelTrails-eip_assoc" {
-  instance_id   = aws_instance.my-ec2.id
-  allocation_id = "eipalloc-098113030e436d0e0"
+resource "aws_eip" "my_eip" {
+  instance = aws_instance.my_ec2.id
+  vpc      = true
 }
 
+resource "aws_eip_association" "my_eip_assoc" {
+  instance_id   = aws_instance.my_ec2.id
+  allocation_id = aws_eip.my_eip.id
+}
 
 output "instance_ip" {
   value = aws_eip.my_eip.public_ip
