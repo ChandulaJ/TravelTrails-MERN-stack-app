422f6342947096fb7060232661c8b4269e7d9838
Chandula Jayathilake <114409648+ChandulaJ@users.noreply.github.com>
2024-06-28 10:58:05 +0530

    Update deploy_server.tf

--- deploy_server.tf
+++ deploy_server.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "my-ec2" {
   ami           = "ami-04b70fa74e45c3917"
   instance_type = "t2.micro"
-  key_name      = "deploy-server-key"
+  key_name      = "traveltrails-deploy-server"
   vpc_security_group_ids = ["sg-08d9dd0b50b76f3a0"]
 
   tags = {
